1ce54583d935a999d81db8a5286173b78bf2ded6
DIEGO-NOTE\diego <diego.wr@gmail.com>
2017-12-05 10:44:57 -0700

    BT26 Add Tick() to PlayerController

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,5 +19,11 @@ public:
 	ATank* GetControlledTank() const;
 	
 	void BeginPlay() override;
+
+	virtual void Tick( float DeltaTime ) override;
+
+private:
+	// Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
 	
 };
