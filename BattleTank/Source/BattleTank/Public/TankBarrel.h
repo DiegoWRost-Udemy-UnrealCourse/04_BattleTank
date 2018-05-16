e82a1566c7c945f88d8f87230913c4b34659a2b9
DIEGO-NOTE\diego <diego.wr@gmail.com>
2018-05-16 10:33:42 -0600

    BT41 Using Clamp() to Limit Values

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -21,7 +21,7 @@ public:
 	
 private:
 	UPROPERTY(EditAnywhere, Category = Setup)
-	float MaxDegreesPerSecond = 20;
+	float MaxDegreesPerSecond = 5;
 
 	UPROPERTY(EditAnywhere, Category = Setup)
 	float MaxElevationDegrees = 40;
